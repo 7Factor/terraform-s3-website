5ca8e889a9f6d34ad6c1d0d33223abba00c15a4d
jduv <jduv@7factor.io>
2019-05-10 14:11:23 -0400

    I should really test things.

--- outputs.tf
+++ outputs.tf
@@ -13,7 +13,7 @@ output "cf_hosted_zone_id" {
   value       = "${aws_cloudfront_distribution.web_distro.hosted_zone_id}"
 }
 
-ouput "s3_bucket_name" {
+output "s3_bucket_name" {
   description = "The name of the bucket. Probably the same as the FQDN, but this so you're 100% sure to get the name."
-  value       = "${aws_s3_bucket.web.name}"
+  value       = "${aws_s3_bucket.web.bucket}"
 }
