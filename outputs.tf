7c7788bbeff50bcb282983e9fc95797e4ba0973a
jduv <jduv@7factor.io>
2019-05-09 22:16:23 -0400

    Adding hosted zone ID to outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "cf_domain" {
   description = "The domain name of the CF distribution."
   value       = "${aws_cloudfront_distribution.web_distro.domain_name}"
 }
+
+output "cf_hosted_zone_id" {
+  description = "The hosted zone of the CF distro. Use this when pointing alias records to the distro."
+  value       = "${aws_cloudfront_distribution.web_distro.hosted_zone_id}"
+}
