4f0a165da36e4878c8b85759dff01a15e973dd3b
jduv <jduv@7factor.io>
2019-05-10 11:58:28 -0400

    Adding S3 bucket name for unabiguous use

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "cf_hosted_zone_id" {
   description = "The hosted zone of the CF distro. Use this when pointing alias records to the distro."
   value       = "${aws_cloudfront_distribution.web_distro.hosted_zone_id}"
 }
+
+ouput "s3_bucket_name" {
+  description = "The name of the bucket. Probably the same as the FQDN, but this so you're 100% sure to get the name."
+  value       = "${aws_s3_bucket.web.name}"
+}
