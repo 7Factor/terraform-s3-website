b26fe2698c3a1856c02d39099e4fbe360ac4a054
jduv <jduv@7factor.io>
2019-05-09 22:14:24 -0400

    Adding cf domain and renaming a few items

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "cloud_formation_distribution_id" {
+output "cf_distribution_id" {
   description = "The ID of the cloud front distro we created. Use this for DNS alias records and invalidation."
   value       = "${aws_cloudfront_distribution.web_distro.id}"
+}
+
+output "cf_domain" {
+  description = "The domain name of the CF distribution."
+  value       = "${aws_cloudfront_distribution.web_distro.domain_name}"
 }
