c84fb66e1ed29b69af0afc48ff2b1736c9a009d9
Teresa Hansen <Teresa.Hansen@7factor.io>
2020-05-18 12:25:50 -0400

    ch6997: Add testing for acl, cors, and policy.

--- integration/spec/main_spec.rb
+++ integration/spec/main_spec.rb
@@ -1,5 +1,56 @@
 require 'awspec'
+require 'hcl/checker'
 
-describe s3_bucket('s3-web-test.7fdev.io') do
+TFVARS = HCL::Checker.parse(File.open('testing.tfvars').read())
+
+describe s3_bucket(TFVARS['primary_fqdn']) do
   it { should exist }
+
+  its(:acl_grants_count) { should eq 2 }
+
+  its(:acl_owner) {
+    should eq 'aws-dev'
+  }
+
+  it {
+    should have_acl_grant(grantee: 'aws-dev', permission: 'FULL_CONTROL')
+  }
+
+  it {
+    should have_cors_rule(
+        allowed_headers: ['*'],
+        allowed_methods: ['GET', 'HEAD'],
+        allowed_origins: TFVARS['origins'],
+        expose_headers: TFVARS['cors_expose_headers'] || [],
+        max_age_seconds: TFVARS['cors_max_age_seconds'] || 3000
+    )
+  }
+  
+  it {
+    should have_policy <<-POLICY
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "",
+                "Effect": "Allow",
+                "Principal": {
+                    "AWS": "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity E2FGOA1GNVRXQH"
+                },
+                "Action": "s3:GetObject",
+                "Resource": "arn:aws:s3:::#{TFVARS['primary_fqdn']}/*"
+            },
+            {
+                "Sid": "",
+                "Effect": "Allow",
+                "Principal": {
+                    "AWS": "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity E2FGOA1GNVRXQH"
+                },
+                "Action": "s3:ListBucket",
+                "Resource": "arn:aws:s3:::#{TFVARS['primary_fqdn']}"
+            }
+        ]
+    }
+    POLICY
+  }
 end
